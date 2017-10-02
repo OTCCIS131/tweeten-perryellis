0680071e6f5188fb3887f8635204cbd2432a3a7e
Cierra Strickland <cs0856663@otc.edu>
2017-10-02 13:19:40 -0500

    Jquery attempts in class

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-let buttons = document.getElementsByClassName("glyphicon glyphicon-heart");
+// let buttons = document.getElementsByClassName("glyphicon glyphicon-heart");
 
-buttons = Array.from(buttons)
-buttons.forEach(button => {
-    button.addEventListener("click", e =>{
-        let currentCount = button.nextSibling.textContent.trim()
-        currentCount = parseInt(currentCount)
-        newCount = currentCount + 1
-        button.nextSibling.textContent = newCount
-    })
-})
+// buttons = Array.from(buttons)
+// buttons.forEach(button => {
+//     button.addEventListener("click", e =>{
+//         let currentCount = button.nextSibling.textContent.trim()
+//         currentCount = parseInt(currentCount)
+//         newCount = currentCount + 1
+//         button.nextSibling.textContent = newCount
+//     })
+// })
+
+$("#like").click(function()){
+    
+}
