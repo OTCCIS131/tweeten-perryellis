205e1047a7950efcd1c94f3e3a7873ba631c015d
Cierra Strickland <cs0856663@otc.edu>
2017-10-02 19:52:20 -0500

    Jquery attempts round 2

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-// let buttons = document.getElementsByClassName("glyphicon glyphicon-heart");
+let buttons = document.getElementsByClassName("glyphicon glyphicon-heart");
 
-// buttons = Array.from(buttons)
-// buttons.forEach(button => {
-//     button.addEventListener("click", e =>{
-//         let currentCount = button.nextSibling.textContent.trim()
-//         currentCount = parseInt(currentCount)
-//         newCount = currentCount + 1
-//         button.nextSibling.textContent = newCount
-//     })
+buttons = Array.from(buttons)
+buttons.forEach(button => {
+    button.addEventListener("click", e =>{
+        let currentCount = button.nextSibling.textContent.trim()
+        currentCount = parseInt(currentCount)
+        newCount = currentCount + 1
+        button.nextSibling.textContent = newCount
+    })
+})
+
+//jquery
+// $("#like").click(function(){
+//     num = parseInt($(".glyphicon glyphicon-heart").text());
+//     $(".glyphicon glyphicon-heart").text(num+1);
 // })
-
-$("#like").click(function()){
-    
-}
